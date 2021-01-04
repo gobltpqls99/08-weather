48b119310365e54b9b631134dda9b8b242192bcb
gobltpqls99 <gobltpqls99@naver.com>
2021-01-04 13:15:55 +0900

    ..

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+//웹사이트 주소체계: https://nodejs.org/dist/latest-v15.x/docs/api/url.html
+
+//openweathermap: ed14b0a28c58544594d7f537cca24e13
+//kakao: 248a9e636f9a29edae7827c3d39b731b
+
+//7days: https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=ed14b0a28c58544594d7f537cca24e13&units=metric
+
+
+//https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=ed14b0a28c58544594d7f537cca24e13&units=metric&exclude=minutely,hourly
